6d09a026372c11a95c6214d7b3fad9c31b5e1d8d
bresche <csh.shop@gmail.com>
2017-11-18 16:45:39 +0100

    + test for grid

--- test/com/company/GridTest.java
+++ test/com/company/GridTest.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class GridTest {
 
+    @Test
+    void testSomething(){
+        //given:
+        Grid grid = new Grid(1,1);
+        grid.setCell(new Cell(),0,0);
 
 
-
+        //expected:
+        assertNotNull(grid);
+    }
 }
