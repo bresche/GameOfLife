c69c3f80fd05e82281302e4c590fa44e49f3fcbd
Gritje <yerodina@gmail.com>
2017-11-24 20:10:30 +0100

    add grid with calculation of cell neighbour count

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -3,27 +3,68 @@ package com.company;
 public class Grid {
 
     private Cell[][] gridArray;
-    private int _rows;
-    private int _columns;
+    private int rows;
+    private int columns;
 
-    public Grid(int rows, int columns)
-    {
-        _rows = rows;
-        _columns=columns;
+    public Grid(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
         gridArray = new Cell[rows][columns];
+
+        initGridWithDeadCells();
     }
-    public void setCell(Cell cell, int row, int col)
-    {
+
+    private void initGridWithDeadCells() {
+        for(int i = 0; i<this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                gridArray[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void addCellAtPosition(Cell cell, int row, int col) {
         gridArray[row][col] = cell;
     }
-    public void setNeighbourCount()
-    {
-            for(int i=0;i<_rows;i++)
-            {
-                for(int j=0;j<_columns;j++)
-                {
-                   // gridArray[i][j].setNeighbourCount();
+
+    public void countNeighboursForAllCellsInGrid() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int numberOfNeighbours = countNeighbourCells(i, j);
+                gridArray[i][j].setNeighbourCount(numberOfNeighbours);
+            }
+        }
+    }
+
+    private int countNeighbourCells(int row, int column) {
+        int neighbourCount = 0;
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+
+                int rowToCheck = row + i;
+                int columnToCheck = column + j;
+
+                if (boundariesAreValid(rowToCheck, columnToCheck)) {
+
+                    if (!isSameCell(row, column, rowToCheck, columnToCheck)) {
+
+                        Cell neighbour = gridArray[rowToCheck][columnToCheck];
+
+                        if (neighbour.isAlive()) {
+                            neighbourCount++;
+                        }
+                    }
                 }
             }
+        }
+        return neighbourCount;
+    }
+
+    private boolean isSameCell(int row, int column, int rowToCheck, int columnToCheck) {
+        return rowToCheck == row && columnToCheck == column;
+    }
+
+    private boolean boundariesAreValid(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 }
--- test/com/company/GridTest.java
+++ test/com/company/GridTest.java
@@ -1,8 +1,54 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GridTest {
 
+    @Test
+    void cellHasNoLivingNeighbours_neighbourCountIsZero() {
+        Grid grid = new Grid(3,3);
+        Cell aLivingCell = new Cell();
+        aLivingCell.setAlive(true);
+        grid.addCellAtPosition(aLivingCell, 1,1);
+        grid.countNeighboursForAllCellsInGrid();
+        assertEquals(0, aLivingCell.getNeighbourCount());
+    }
 
+    @Test
+    void cellHasThreeLivingNeighbours_neighbourCountIsThree() {
+        Grid grid = new Grid(3,3);
+        Cell aLivingCell = new Cell();
+        aLivingCell.setAlive(true);
+        Cell theLivingCellToCheck = new Cell();
+        theLivingCellToCheck.setAlive(true);
+        grid.addCellAtPosition(aLivingCell, 0,0);
+        grid.addCellAtPosition(aLivingCell, 1,0);
+        grid.addCellAtPosition(theLivingCellToCheck, 1,1);
+        grid.addCellAtPosition(aLivingCell, 2,2);
+        grid.countNeighboursForAllCellsInGrid();
+        assertEquals(3, theLivingCellToCheck.getNeighbourCount());
+    }
 
+    @Test
+    void cellHasEightLivingNeighbours_neighbourCountIsEight() {
+        Grid grid = new Grid(3,3);
+        Cell aLivingCell = new Cell();
+        aLivingCell.setAlive(true);
+        Cell theLivingCellToCheck = new Cell();
+        theLivingCellToCheck.setAlive(true);
+        grid.addCellAtPosition(aLivingCell, 0,0);
+        grid.addCellAtPosition(aLivingCell, 1,0);
+        grid.addCellAtPosition(aLivingCell, 2,0);
+        grid.addCellAtPosition(aLivingCell, 0,1);
+        grid.addCellAtPosition(theLivingCellToCheck, 1,1);
+        grid.addCellAtPosition(aLivingCell, 2,1);
+        grid.addCellAtPosition(aLivingCell, 0,2);
+        grid.addCellAtPosition(aLivingCell, 1,2);
+        grid.addCellAtPosition(aLivingCell, 2,2);
+        grid.countNeighboursForAllCellsInGrid();
+        assertEquals(8, theLivingCellToCheck.getNeighbourCount());
+    }
 
 }
